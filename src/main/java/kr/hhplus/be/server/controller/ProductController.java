b1575e7fe2515f0fedef55588cadd22bf2abd7c3
seungjoon <dltmd310@gmail.com>
2025-07-24 07:18:19 +0900

    dto 객체명변경
    ResponseProductList -> ResponseProduct

--- src/main/java/kr/hhplus/be/server/controller/ProductController.java
+++ src/main/java/kr/hhplus/be/server/controller/ProductController.java
@@ -2,7 +2,7 @@ package kr.hhplus.be.server.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import kr.hhplus.be.server.dto.product.ResponseProductList;
+import kr.hhplus.be.server.dto.product.ResponseProduct;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,11 +17,11 @@ public class ProductController {
 
     @Operation(summary = "상품 list를 조회")
     @GetMapping("/list")
-    public ResponseEntity<List<ResponseProductList>> getProductList() {
+    public ResponseEntity<List<ResponseProduct>> getProductList() {
 
-        List<ResponseProductList> productListDto = List.of(
-                new ResponseProductList(1,"상품1",3,1000,"의류"),
-                new ResponseProductList(2,"상품2",5,2000,"침구류")
+        List<ResponseProduct> productListDto = List.of(
+                new ResponseProduct(1,"상품1",3,1000,"의류"),
+                new ResponseProduct(2,"상품2",5,2000,"침구류")
                 );
 
         return ResponseEntity.ok(productListDto);
@@ -30,14 +30,14 @@ public class ProductController {
 
     @Operation(summary = "상위 5개 상품 조회")
     @GetMapping("/list/top5")
-    public ResponseEntity<List<ResponseProductList>> getProductListTop5() {
+    public ResponseEntity<List<ResponseProduct>> getProductListTop5() {
 
-        List<ResponseProductList> productListDto = List.of(
-                new ResponseProductList(1,"상품1",3,1000,"의류"),
-                new ResponseProductList(2,"상품2",4,2000,"침구류"),
-                new ResponseProductList(3,"상품3",5,3000,"가구류"),
-                new ResponseProductList(4,"상품4",1,6100,"필기구"),
-                new ResponseProductList(5,"상품5",7,5000,"의류")
+        List<ResponseProduct> productListDto = List.of(
+                new ResponseProduct(1,"상품1",3,1000,"의류"),
+                new ResponseProduct(2,"상품2",4,2000,"침구류"),
+                new ResponseProduct(3,"상품3",5,3000,"가구류"),
+                new ResponseProduct(4,"상품4",1,6100,"필기구"),
+                new ResponseProduct(5,"상품5",7,5000,"의류")
                 );
 
         return ResponseEntity.ok(productListDto);
